eb206a391bdd935a5411533b3a5aa010638732e9
kuldeeptiwari-gif <kuldeep.tiwari@luminnoverse.com>
2026-06-23 17:41:31 +0530

    Initial commit

---
+++ Data/AppdbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Task_Management_Web.Data
+{
+    public class AppdbContext : DbContext
+    {
+        public AppdbContext(DbContextOptions options) :base(options) { }
+    }
+}
