b6a2f570a9c1af1a110fa324904da88b7d275e33
kuldeeptiwari-gif <kuldeep.tiwari@luminnoverse.com>
2026-06-23 19:25:36 +0530

    commit the code

---
+++ Controllers/DeveloperController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task_Management_Web.Controllers
+{
+    public class DeveloperController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/SupportController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task_Management_Web.Controllers
+{
+    public class SupportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
