01f3764ba8604ceb6b4039d05207d4a2480910a9
kuldeeptiwari-gif <kuldeep.tiwari@luminnoverse.com>
2026-06-24 21:55:38 +0530

    commit the code

--- Migrations/20260624162402_Add-new-Roles.Designer.cs
+++ Migrations/20260624162402_Add-new-Roles.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Task_Management_Web.Data;
 
@@ -11,9 +12,11 @@ using Task_Management_Web.Data;
 namespace Task_Management_Web.Migrations
 {
     [DbContext(typeof(AppdbContext))]
-    partial class AppdbContextModelSnapshot : ModelSnapshot
+    [Migration("20260624162402_Add-new-Roles")]
+    partial class AddnewRoles
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Models/Roles.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Task_Management_Web.Models
+{
+    public class Roles
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
